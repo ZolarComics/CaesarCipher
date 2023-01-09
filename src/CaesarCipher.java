51b345ad00da45dd99acc73dcdae934a618b757f
ZolarComics <srgjzln@gmail.com>
2023-01-09 14:17:39 +1000

    Added logic for user

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,5 +1,5 @@
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
@@ -8,21 +8,68 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class CaesarCipher {
 
     static List<Character> fileText = getArrayList();
 
     public static void main(String[] args) {
-//        System.out.println(-3 * (-1));"C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt"
-        encrypt("C:\\Users\\srgjz\\OneDrive\\Рабочий стол\\encryptingTest\\text.txt",1);
+//      C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt
+//      Перенести мейн в отдельный класс, в логику ниже прописать в констркуторе.
+        String uri;
+        int key = 0;
+        try (Scanner in = new Scanner(System.in)) {
+
+            while (true) {
+                System.out.println("Введите путь к файлу:");
+                uri = in.nextLine();
+                File file = new File(uri);
+                if (file.exists()) {
+                    break;
+                } else {
+                    System.out.println("Такого файла не сущечвует");
+                }
+            }
+
+            while (key <= 0) {
+                System.out.println("Сдвиг на (натуральное не отрицательное число):");
+                key = in.nextInt();
+                if (key <= 0) {
+                    System.out.println("Число должно быть положительным");
+                }
+            }
+
+            key = shift(key);
+
+            System.out.println(key);
+
+            encrypt(uri,key);
+        }
     }
+
+//    Нужно сделать так, чтобы если символ введен неверно программа не продолжалась, а ожидалла корректного ввода.
+    static public int shift(int key) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Сдвиг в право\\лево (L\\R):");
+            String sdvig = in.nextLine();
+            switch (sdvig) {
+                case "L" -> key = key * (-1);
+                case "R" -> {}
+                default -> System.out.println("Не верный ввод");
+            }
+            return key;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 //    Можно попробовать упростить, реализовав метод с одним buffer'ом.
-//    Нужно уточнить, можно ли вызывать метод get у buffer без flip.
 //    Надо найти вариант, как реализовать этот метод так, чтобы не повторялся код, иначе создавать отдельный метод с таким же
 //    кодом было бы иррационально.
-    static public void encrypt(String url, int key) {
-        try (FileChannel text = FileChannel.open(Paths.get(url))) {
+    static public void encrypt(String uri, int key) {
+        try (FileChannel text = FileChannel.open(Paths.get(uri))) {
             ByteBuffer readBuffer = ByteBuffer.allocate((int) text.size());
 
             StringBuilder builder = new StringBuilder();
@@ -32,7 +79,6 @@ public class CaesarCipher {
 
             while (readBuffer.hasRemaining()) {
 //               Тут нужно доделать проверку, чтобы если index > fileText.length начинался с начала
-//                 + key
 //                builder.append(fileText.get(fileText.indexOf((char) readBuffer.get())+ key));
                 builder.append(readBuffer.getChar());
 //                System.out.println();
@@ -44,7 +90,7 @@ public class CaesarCipher {
             ByteBuffer writeBuffer = ByteBuffer.allocate(encryptedText.getBytes().length);
             writeBuffer.put(encryptedText.getBytes());
 
-            Path encryptedFile = Files.createFile(Path.of(getNewFileName(url)));
+            Path encryptedFile = Files.createFile(Path.of(getNewFileName(uri)));
             if(Files.notExists(encryptedFile)){
                 Files.createFile(encryptedFile);
             }
@@ -53,6 +99,7 @@ public class CaesarCipher {
                 partInputChannel.write(writeBuffer);
                 writeBuffer.clear();
             }
+            System.out.println("Файл зашифрован");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -60,7 +107,7 @@ public class CaesarCipher {
 
     public static String getNewFileName(String oldFileName) {
         int dotIndex = oldFileName.lastIndexOf(".");
-        return oldFileName.substring(0, dotIndex) + "encrypted" + oldFileName.substring(dotIndex);
+        return oldFileName.substring(0, dotIndex) + "Encrypted" + oldFileName.substring(dotIndex);
     }
 
      public static List<Character> getArrayList() {
