4f79fb676ad688624e2415bbe01bada0e40d3fa0
ZolarComics <srgjzln@gmail.com>
2023-01-13 10:15:34 +1000

    c\d for files add (but don't work)

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,15 +1,10 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,16 +12,18 @@ import java.util.Scanner;
 public class CaesarCipher {
 
     static List<Character> simbolsText = getArrayList();
+    static String uri;
+    static int key = 0;
+    static boolean isLeft;
+    static boolean isDecoding;
 
 //    ДЕДЛАЙН ДЛЯ ЭТОГО ПРОЕКТА 21 ЯНВАРЯ (22 КРАЙ)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     public static void main(String[] args) {
 //      C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt
 //      Перенести мейн в отдельный класс, в логику ниже прописать в констркуторе.
+//      Вынести все цыклы в отдельные методы, чтобы в конструкторе они только вызыввались
         System.out.println(simbolsText.size());
-        String uri;
-        int key = 0;
-        boolean isMinus;
         try (Scanner in = new Scanner(System.in)) {
 
             while (true) {
@@ -47,26 +44,26 @@ public class CaesarCipher {
                     System.out.println("Число должно быть положительным и не должно равняться нулю");
                 }
             }
-
-            isMinus = shift();
+            isLeft = getShift();
+            isDecoding = getMethod();
 
             System.out.println(key);
-            System.out.println(isMinus);
+            System.out.println(isLeft);
 
-            encrypt(uri,key, isMinus);
+            encrypt(uri,key, isLeft, isDecoding);
         }
     }
 
-    static public boolean shift() {
+    static public boolean getMethod() {
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Сдвиг в право\\лево (l\\r):");
+            System.out.println("Шифровка\\Разшифровка (c\\d):");
             boolean result = false;
-            String sdvig = "";
-            while (!sdvig.equals("l") && !sdvig.equals("r")) {
-                sdvig = in.nextLine();
-                if (!sdvig.equals("l") && !sdvig.equals("r")) {
+            String methodToDo = "";
+            while (!methodToDo.equals("c") && !methodToDo.equals("d")) {
+                methodToDo = in.nextLine();
+                if (!methodToDo.equals("c") && !methodToDo.equals("d")) {
                     System.out.println("Неверный ввод");
-                }else if (sdvig.equals("l")) {
+                }else if (methodToDo.equals("d")) {
                     result = true;
                 }
             }
@@ -76,14 +73,32 @@ public class CaesarCipher {
         }
     }
 
-//    Надо найти вариант, как реализовать этот метод так, чтобы не повторялся код, иначе создавать отдельный метод с таким же
-//    кодом было бы иррационально.
+    static public boolean getShift() {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Сдвиг в право\\лево (l\\r):");
+            boolean result = false;
+            String shiftSimbol = "";
+            while (!shiftSimbol.equals("l") && !shiftSimbol.equals("r")) {
+                shiftSimbol = in.nextLine();
+                if (!shiftSimbol.equals("l") && !shiftSimbol.equals("r")) {
+                    System.out.println("Неверный ввод");
+                }else if (shiftSimbol.equals("l")) {
+                    result = true;
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    Вынести код с нправлениями сдвига в отдельные методы и сделать проверку на шифровку и на дешифровку
 
 //    Вместо ByteBuffer writeBuffer попробовать использовать буферы, которые сразу хронят char
 //    Попробовать использовать для чтения файла класс Files, так как там есть метод который читает весь файл и овзвращает его в виде строкиё
 
 //    C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt
-    static public void encrypt(String uri, int key, boolean rotation) {
+    static public void encrypt(String uri, int key, boolean rotation, boolean isDecoding) {
         try (Reader file = new FileReader(uri)) {
             BufferedReader buffReader = new BufferedReader(file);
 
@@ -93,21 +108,17 @@ public class CaesarCipher {
             }
             System.out.println(charList);
 
-            if (rotation){
-                for (int i = 0; i < charList.size(); i++) {
-                    int encryptedSimbolIndex = simbolsText.indexOf(charList.get(i)) - key;
-                        while (encryptedSimbolIndex < 0) {
-                            encryptedSimbolIndex = encryptedSimbolIndex + simbolsText.size();
-                        }
-                    charList.set(i, simbolsText.get(encryptedSimbolIndex));
+            if (isDecoding) {
+                if (rotation) {
+                    RightCoding(charList);
+                } else {
+                    LeftCoding(charList);
                 }
             } else {
-                for (int i = 0; i < charList.size(); i++) {
-                    int encryptedSimbolIndex = simbolsText.indexOf(charList.get(i)) + key;
-                        while (encryptedSimbolIndex >= simbolsText.size()) {
-                            encryptedSimbolIndex = encryptedSimbolIndex - simbolsText.size();
-                        }
-                    charList.set(i, simbolsText.get(encryptedSimbolIndex));
+                if (rotation){
+                    LeftCoding(charList);
+                } else {
+                    RightCoding(charList);
                 }
             }
 
@@ -136,13 +147,39 @@ public class CaesarCipher {
         }
     }
 
+    public static void LeftCoding(List<Character> inputText) {
+        for (int i = 0; i < inputText.size(); i++) {
+            int encryptedSimbolIndex = simbolsText.indexOf(inputText.get(i)) - key;
+            while (encryptedSimbolIndex < 0) {
+                encryptedSimbolIndex = encryptedSimbolIndex + simbolsText.size();
+            }
+            inputText.set(i, simbolsText.get(encryptedSimbolIndex));
+        }
+    }
+    public static void RightCoding(List<Character> inputText) {
+        for (int i = 0; i < inputText.size(); i++) {
+            int encryptedSimbolIndex = simbolsText.indexOf(inputText.get(i)) + key;
+            while (encryptedSimbolIndex >= simbolsText.size()) {
+                encryptedSimbolIndex = encryptedSimbolIndex - simbolsText.size();
+            }
+            inputText.set(i, simbolsText.get(encryptedSimbolIndex));
+        }
+    }
+
+//    Изменить метод так, чтобы в зависимости от действия создавалось определенное имя (coded\decoded)
     public static String getNewFileName(String oldFileName) {
         int dotIndex = oldFileName.lastIndexOf(".");
-        return oldFileName.substring(0, dotIndex) + "Encrypted" + oldFileName.substring(dotIndex);
+        String newFileNmae = oldFileName.substring(0, dotIndex) + "Coded" + oldFileName.substring(dotIndex);
+        int count = 0;
+        while (Files.exists(Path.of(newFileNmae))) {
+            count++;
+            newFileNmae = oldFileName.substring(0, dotIndex) + "Coded" + count + oldFileName.substring(dotIndex);
+        }
+        return newFileNmae;
     }
 
      public static List<Character> getArrayList() {
-        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-! ?";
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\":-! ?";
         ArrayList<Character> chars = new ArrayList<>();
         for (char ch: alphabet.toCharArray()) {
             chars.add(ch);
