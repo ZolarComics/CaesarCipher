20a9882c3e0bbb94a172acf57158007714019ada
ZolarComics <srgjzln@gmail.com>
2023-01-13 20:01:00 +1000

    coding/decoding done, start BruteForce

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,9 +1,4 @@
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,23 +6,36 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
-    static List<Character> simbolsText = getArrayList();
+    static List<Character> simbolsText = Main.getArrayList();
     static String uri;
-    static int key = 0;
+    static int key;
     static boolean isLeft;
     static boolean isDecoding;
 
 //    ДЕДЛАЙН ДЛЯ ЭТОГО ПРОЕКТА 21 ЯНВАРЯ (22 КРАЙ)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    public static void main(String[] args) {
 //      C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt
-//      Перенести мейн в отдельный класс, в логику ниже прописать в констркуторе.
-//      Вынести все цыклы в отдельные методы, чтобы в конструкторе они только вызыввались
-        System.out.println(simbolsText.size());
-        try (Scanner in = new Scanner(System.in)) {
 
+//      System.out.println((int) '.'); // Способ, с помощью которого можно получить числовое значение символа.
+//      System.out.println(1040 + 31); // Соответсвенно, знаюя что можно сделтаь так, нужно переписать весь код с учетом этих знаний.
+//      System.out.println((int) 'Я'); // Хотя по предварительнымм выводам рзницы особой нет, что там есть ключи, что там индексы.
+
+    public static void getCaesarCipherData() {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Шифровка\\Разшифровка (c\\d):");
+            isDecoding = false;
+            String methodToDo = "";
+            while (!methodToDo.equals("c") && !methodToDo.equals("d")) {
+                methodToDo = in.nextLine();
+                if (!methodToDo.equals("c") && !methodToDo.equals("d")) {
+                    System.out.println("Неверный ввод");
+                }else if (methodToDo.equals("d")) {
+                    isDecoding = true;
+                }
+            }
+
+            System.out.println("Введите путь к файлу:");
             while (true) {
-                System.out.println("Введите путь к файлу:");
                 uri = in.nextLine();
                 File file = new File(uri);
                 if (file.exists()) {
@@ -37,111 +45,56 @@ public class CaesarCipher {
                 }
             }
 
-            while (key <= 0) {
-                System.out.println("Сдвиг на (натуральное не отрицательное число):");
+            System.out.println("Сдвиг на (натуральное не отрицательное число):");
+            while (true) {
                 key = in.nextInt();
                 if (key <= 0) {
                     System.out.println("Число должно быть положительным и не должно равняться нулю");
+                } else {
+                    break;
                 }
             }
-            isLeft = getShift();
-            isDecoding = getMethod();
 
-            System.out.println(key);
-            System.out.println(isLeft);
-
-            encrypt(uri,key, isLeft, isDecoding);
-        }
-    }
-
-    static public boolean getMethod() {
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Шифровка\\Разшифровка (c\\d):");
-            boolean result = false;
-            String methodToDo = "";
-            while (!methodToDo.equals("c") && !methodToDo.equals("d")) {
-                methodToDo = in.nextLine();
-                if (!methodToDo.equals("c") && !methodToDo.equals("d")) {
-                    System.out.println("Неверный ввод");
-                }else if (methodToDo.equals("d")) {
-                    result = true;
-                }
-            }
-            return result;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static public boolean getShift() {
-        try (Scanner in = new Scanner(System.in)) {
             System.out.println("Сдвиг в право\\лево (l\\r):");
-            boolean result = false;
-            String shiftSimbol = "";
+            isLeft = false;
+            String shiftSimbol = in.nextLine();
             while (!shiftSimbol.equals("l") && !shiftSimbol.equals("r")) {
                 shiftSimbol = in.nextLine();
                 if (!shiftSimbol.equals("l") && !shiftSimbol.equals("r")) {
                     System.out.println("Неверный ввод");
                 }else if (shiftSimbol.equals("l")) {
-                    result = true;
+                    isLeft = true;
                 }
             }
-            return result;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
-
-//    Вынести код с нправлениями сдвига в отдельные методы и сделать проверку на шифровку и на дешифровку
 
 //    Вместо ByteBuffer writeBuffer попробовать использовать буферы, которые сразу хронят char
 //    Попробовать использовать для чтения файла класс Files, так как там есть метод который читает весь файл и овзвращает его в виде строкиё
 
 //    C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\text.txt
-    static public void encrypt(String uri, int key, boolean rotation, boolean isDecoding) {
-        try (Reader file = new FileReader(uri)) {
-            BufferedReader buffReader = new BufferedReader(file);
-
-            List<Character> charList = new ArrayList<>();
-            while (buffReader.ready()) {
-                charList.add((char) buffReader.read());
-            }
-            System.out.println(charList);
+//    C:\Users\srgjz\OneDrive\Рабочий стол\encryptingTest\textCoded.txt
+    static public void encrypt() {
+        try {
+            getCaesarCipherData();
+            List<Character> fileText = FileWork.getCharList(uri);
 
             if (isDecoding) {
-                if (rotation) {
-                    RightCoding(charList);
+                if (isLeft) {
+                    RightCoding(fileText);
                 } else {
-                    LeftCoding(charList);
+                    LeftCoding(fileText);
                 }
             } else {
-                if (rotation){
-                    LeftCoding(charList);
+                if (isLeft){
+                    LeftCoding(fileText);
                 } else {
-                    RightCoding(charList);
+                    RightCoding(fileText);
                 }
             }
 
-            StringBuilder builder = new StringBuilder();
-            for (char aChar: charList) {
-                builder.append(aChar);
-            }
-            String encryptedText = builder.toString();
-            System.out.println(encryptedText);
+            FileWork.setEncryptFile(fileText, uri, isDecoding);
 
-            ByteBuffer writeBuffer = ByteBuffer.allocate(encryptedText.getBytes().length);
-            writeBuffer.put(encryptedText.getBytes());
-
-            Path encryptedFile = Files.createFile(Path.of(getNewFileName(uri)));
-            if(Files.notExists(encryptedFile)){
-                Files.createFile(encryptedFile);
-            }
-            try (FileChannel partInputChannel = FileChannel.open(encryptedFile, StandardOpenOption.WRITE)) {
-                writeBuffer.flip();
-                partInputChannel.write(writeBuffer);
-                writeBuffer.clear();
-            }
-            System.out.println("Файл зашифрован");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -149,6 +102,8 @@ public class CaesarCipher {
 
     public static void LeftCoding(List<Character> inputText) {
         for (int i = 0; i < inputText.size(); i++) {
+            if (!simbolsText.contains(inputText.get(i)))
+                continue;
             int encryptedSimbolIndex = simbolsText.indexOf(inputText.get(i)) - key;
             while (encryptedSimbolIndex < 0) {
                 encryptedSimbolIndex = encryptedSimbolIndex + simbolsText.size();
@@ -158,6 +113,8 @@ public class CaesarCipher {
     }
     public static void RightCoding(List<Character> inputText) {
         for (int i = 0; i < inputText.size(); i++) {
+            if (!simbolsText.contains(inputText.get(i)))
+                continue;
             int encryptedSimbolIndex = simbolsText.indexOf(inputText.get(i)) + key;
             while (encryptedSimbolIndex >= simbolsText.size()) {
                 encryptedSimbolIndex = encryptedSimbolIndex - simbolsText.size();
@@ -166,24 +123,5 @@ public class CaesarCipher {
         }
     }
 
-//    Изменить метод так, чтобы в зависимости от действия создавалось определенное имя (coded\decoded)
-    public static String getNewFileName(String oldFileName) {
-        int dotIndex = oldFileName.lastIndexOf(".");
-        String newFileNmae = oldFileName.substring(0, dotIndex) + "Coded" + oldFileName.substring(dotIndex);
-        int count = 0;
-        while (Files.exists(Path.of(newFileNmae))) {
-            count++;
-            newFileNmae = oldFileName.substring(0, dotIndex) + "Coded" + count + oldFileName.substring(dotIndex);
-        }
-        return newFileNmae;
-    }
 
-     public static List<Character> getArrayList() {
-        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,\":-! ?";
-        ArrayList<Character> chars = new ArrayList<>();
-        for (char ch: alphabet.toCharArray()) {
-            chars.add(ch);
-        }
-        return chars;
-    }
 }
